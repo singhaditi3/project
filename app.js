33dff62052a0bcf19935d811d5670839519aaf62
singhaditi3 <virusaditisingh@gmail.com>
2020-07-20 20:48:00 +0530

    make it better

--- app.js
+++ app.js
@@ -26,41 +26,9 @@ app.get("/movieresults", function(req,res){
         }
     });
 });
-//............................................................................................//
+//---------------------------------------------------//
 
-//for news service//
-app.get("/newssearch", function(req,res){
-    res.render("newssearch.ejs");
-})
 
-app.get("/news", function(req,res){
-    var query = req.query.news
-    var url = "http://newsapi.org/v2/top-headlines?sources=" + query + "&apiKey=b64b43566b8b4dbd858c5ba7b30414ff"
-    request(url, function(error, response, body){
-        if(!error && response.statusCode == 200){
-            var data = JSON.parse(body);
-            res.render("news.ejs", {data: data});
-        }
-    });
+app.listen(3000, function(){
+    console.log("started");
 });
-//.......................................................................................................................//
-
-//for weather service//
-app.get("/weathersearch", function(req,res){
-    res.render("weathersearch.ejs");
-});
-
-app.get("/weather", function(req,res){
-    var query = req.query.weather
-    var query1 = req.query.country
-    var url = "https://api.weatherbit.io/v2.0/current?city=" + query + "&country=" + query1 + "&key=e0fcf59b0f284e95908929f57513b9d0"
-    request(url, function(error, response, body){
-        if(!error && response.statusCode == 200){
-            var data = JSON.parse(body);
-            res.render("weather.ejs", {data: data});
-        }
-    });
-});
-//.............................................................................................................................................
-
-app.listen(process.env.PORT, process.env.IP);
